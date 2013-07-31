734127c79e9b2c46d2784ede227ace6ec21b9dfe
Ivan Gadzhega <ivgadzhega@gmail.com>
2013-07-30 18:17:55 -0700

    Replaced Random() instance with MathUtils.random()

--- sources/Main/src/main/net/ivang/axonix/main/actors/game/background/Background.java
+++ sources/Main/src/main/net/ivang/axonix/main/actors/game/background/Background.java
@@ -18,11 +18,10 @@ package net.ivang.axonix.main.actors.game.background;
 
 import com.badlogic.gdx.graphics.Color;
 import com.badlogic.gdx.graphics.g2d.TextureRegion;
+import com.badlogic.gdx.math.MathUtils;
 import com.badlogic.gdx.scenes.scene2d.Actor;
 import com.badlogic.gdx.scenes.scene2d.Group;
 import com.badlogic.gdx.scenes.scene2d.ui.Skin;
-
-import java.util.Random;
 
 /**
  * @author Ivan Gadzhega
@@ -34,17 +33,14 @@ public class Background extends Group {
     private final static byte ACTORS_NUM = 50;
     private final static int MAX_HEIGHT = 1080;
 
-    private Random random;
-
     public Background(Skin skin) {
-        this.random = new Random();
         // init flares
         TextureRegion texture = skin.getRegion(TEXTURE_FLARE);
         Color color = new Color(1, 1, 1, 0.2f);
         int x = -texture.getRegionWidth() - 1;
         int y = -texture.getRegionHeight() - 1;
         for (int i = 0; i < ACTORS_NUM; i++) {
-            addActor(new BackgroundFlare(x, y, random.nextFloat(), 0, color, texture));
+            addActor(new BackgroundFlare(x, y, MathUtils.random(), 0, color, texture));
         }
     }
 
@@ -61,12 +57,12 @@ public class Background extends Group {
         for (Actor flare : getChildren()) {
             if (force || (flare.getX() > width) || (flare.getX() < -flare.getWidth())
                     || (flare.getY() < -flare.getHeight()) || (flare.getY() > height) ) {
-                float x = width * random.nextFloat();
-                float y = height * random.nextFloat();
-                float angle = random.nextInt(360);
-                Color color = new Color(random.nextFloat(), random.nextFloat(), random.nextFloat(), 0.2f);
+                float x = width * MathUtils.random();
+                float y = height * MathUtils.random();
+                float angle = MathUtils.random(359);
+                Color color = new Color(MathUtils.random(), MathUtils.random(), MathUtils.random(), 0.2f);
                 if (force) {
-                    float scale = random.nextFloat() * scaleCorrection;
+                    float scale = MathUtils.random() * scaleCorrection;
                     ((BackgroundFlare) flare).update(x, y, angle, scale, color);
                 } else {
                     ((BackgroundFlare) flare).update(x, y, angle, color);
